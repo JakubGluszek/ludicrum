b2b1ddba0642263648470208d4788bb0ad2adc63
JacobG03 <jacobgluszek03@gmail.com>
2022-07-25 10:17:22 +0200

    working leaflet map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("../components/Map"), { ssr: false })
 
 const Home: NextPage = () => {
   return (
@@ -8,11 +11,22 @@ const Home: NextPage = () => {
         <title>Ludicrum</title>
         <meta name="description" content="A platform where you can find street performances around you." />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+          integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+          crossOrigin=""
+        />
       </Head>
 
       <main className="">
         <h1>Find street performances around you.</h1>
       </main>
+
+      <div className="w-full h-96 flex">
+        <Map />
+      </div>
+
     </>
   );
 };
