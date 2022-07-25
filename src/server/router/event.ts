7e0b128225ccab323799d3de8140d3773fecd5c3
JacobG03 <jacobgluszek03@gmail.com>
2022-07-25 17:48:41 +0200

    event mutations

--- src/server/router/event.ts
+++ src/server/router/event.ts
@@ -5,7 +5,7 @@ import { createRouter } from "./context";
 const eventRouter = createRouter()
   .query("all-events", {
     async resolve({ ctx }) {
-      const events = ctx.prisma.event.findMany();
+      const events = await ctx.prisma.event.findMany();
 
       return events;
     },
@@ -15,7 +15,7 @@ const eventRouter = createRouter()
       id: z.string().cuid(),
     }),
     async resolve({ ctx, input }) {
-      const event = ctx.prisma.event.findFirst({
+      const event = await ctx.prisma.event.findFirst({
         where: {
           id: input.id,
         },
@@ -27,7 +27,7 @@ const eventRouter = createRouter()
               image: true,
             },
           },
-          EventReview: {
+          reviews: {
             select: {
               id: true,
               body: true,
@@ -58,6 +58,78 @@ const eventRouter = createRouter()
         session: { ...ctx.session, user: ctx.session.user },
       },
     });
+  })
+  .mutation("create", {
+    input: z.object({
+      name: z.string().min(6).max(64),
+      description: z.string().min(0).max(512),
+      lat: z.string(),
+      lng: z.string(),
+      date: z.date(),
+    }),
+    async resolve({ ctx, input }) {
+      const event = await ctx.prisma.event.create({
+        data: {
+          ...input,
+          userId: ctx.session.user.id!,
+        },
+      });
+
+      return event;
+    },
+  })
+  .mutation("update", {
+    input: z.object({
+      id: z.string().cuid(),
+      data: z.object({
+        name: z.string().min(6).max(64).optional(),
+        description: z.string().min(0).max(512).optional(),
+        lat: z.string().optional(),
+        lng: z.string().optional(),
+        date: z.date().optional(),
+      }),
+    }),
+    async resolve({ ctx, input }) {
+      const event = await ctx.prisma.event.update({
+        where: {
+          userId: ctx.session.user.id,
+          id: input.id,
+        },
+        data: input.data,
+      });
+
+      return event;
+    },
+  })
+  .mutation("generate-review-code", {
+    input: z.object({
+      id: z.string().cuid(),
+    }),
+    async resolve({ ctx, input }) {
+      let r = (Math.random() + 1).toString(36).substring(7);
+
+      await ctx.prisma.event.update({
+        where: {
+          id: input.id,
+          userId: ctx.session.user.id!,
+        },
+        data: {
+          reviewCode: r,
+        },
+      });
+
+      return r;
+    },
+  })
+  .mutation("create-review", {
+    input: z.object({
+      eventId: z.string().cuid(),
+      body: z.string().max(256).optional(),
+      rating: z.number(),
+    }),
+    async resolve({ ctx, input }) {
+      return;
+    },
   });
 
 export default eventRouter;
