90a4357ab2dc954ba542276b6154c69863927e68
JacobG03 <jacobgluszek03@gmail.com>
2022-07-26 14:16:33 +0200

    custom tailwind themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,4 +5,32 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        "custom-dark": {
+          primary: "#1d4ed8",
+          secondary: "#0369a1",
+          accent: "#22c55e",
+          neutral: "#f3f4f6",
+          "base-100": "#111827",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+        "custom-light": {
+          primary: "#1d4ed8",
+          secondary: "#0369a1",
+          accent: "#22c55e",
+          neutral: "#111827",
+          "base-100": "#f3f4f6",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
 };
